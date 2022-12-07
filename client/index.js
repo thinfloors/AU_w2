3f31a7253833a8d51b396b6ac0d7467ee90b78c2
thinfloors <gorbash3000@gmail.com>
2022-12-07 21:54:44 +1100

    Week 2 solution

--- client/index.js
+++ client/index.js
@@ -1,17 +1,31 @@
-const axios = require('axios');
-const niceList = require('../utils/niceList.json');
-const MerkleTree = require('../utils/MerkleTree');
+const axios = require("axios");
+const niceList = require("../utils/niceList.json");
+const MerkleTree = require("../utils/MerkleTree");
 
-const serverUrl = 'http://localhost:1225';
+const serverUrl = "http://localhost:1225";
+
+// Names provided for use, first name is on the niceList, second is not
+// "Gilbert Morar"
+// "Aldous Buxley"
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+    // TODO: how do we prove to the server we're on the nice list?
+    const name = "Gilbert Morar";
+    const index = niceList.findIndex((n) => n === name);
 
-  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
-  });
+    const merkle = new MerkleTree(niceList);
+    const root = merkle.getRoot();
+    const proof = merkle.getProof(index);
 
-  console.log({ gift });
+    const { data: gift } = await axios.post(`${serverUrl}/gift`, {
+        name: name,
+        proof: proof,
+    });
+
+    console.log({ gift });
 }
 
 main();
+
+// In this file:
+// Need to prove to the server that some name is in the MERKLE_ROOT on the server
